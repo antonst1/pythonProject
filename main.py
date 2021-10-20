accb3baef4fd5a38cb7616780d3ad3f3ba4b173d
antonst1 <antonst1@yandex.ru>
2021-10-20 18:00:52 +0400

    Test message

---
+++ main.py
@@ -0,0 +1,13 @@
+# s = 'PYTHON'
+# print(s[::-1])  # переворот строки
+
+# Форматирование строки
+
+# name = 'Anthony'
+# age = 42
+# print('My name is {0}. I\'m {1}'.format(name, age))
+
+# f-strings
+# print(f'My name is {name}. I\'m {age}')
+
+age = int(input('Сколько вам лет?'))
